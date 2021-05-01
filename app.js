0c359d826ff116e54e80f8edae69c417460f384c
Ole Tjernstad <ott556@gmail.com>
2021-05-01 16:29:59 +0200

    serve static files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.set("view engine", "pug");
 app.set("views", "./src/views");
 
 app.use(routes);
+app.use("/static", express.static("public"));
 
 app.listen(3000, () => {
   console.log("Running on localhost:3000!");
