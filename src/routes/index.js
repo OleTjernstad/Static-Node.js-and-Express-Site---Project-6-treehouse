781387219adb64c6293d575b65dd7e71adf1b060
Ole Tjernstad <ott556@gmail.com>
2021-05-02 11:10:22 +0200

    project page

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,12 @@ const data = require("../../data.json");
 router.get("/", (req, res) => {
     res.render("index", data);
 });
+router.get("/project/:id", (req, res) => {
+    const project = data.projects.find(
+        (project) => project.id == req.params.id
+    );
+    res.render("project", project);
+});
 router.get("/about", (req, res) => {
     res.render("about");
 });
