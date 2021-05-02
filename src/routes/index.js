7c2e8e66cbc79f6dacb98c79e84b4e3cf822cf4f
Ole Tjernstad <ott556@gmail.com>
2021-05-02 20:25:17 +0200

    only iteger in project route

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ const data = require("../../data.json");
 router.get("/", (req, res) => {
     res.render("index", data);
 });
-router.get("/project/:id", (req, res) => {
+router.get("/project/:id(\\d+)", (req, res) => {
     const project = data.projects.find(
         (project) => project.id == req.params.id
     );
