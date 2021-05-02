759c81cd3e5ea511abfad0c19ff7b41c35720311
Ole Tjernstad <ott556@gmail.com>
2021-05-02 22:23:55 +0200

    console.log error

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.locals.error = err;
-
+    console.error(err.message);
     if (err.status == 404) {
         res.render("errors/page-not-found");
         return;
